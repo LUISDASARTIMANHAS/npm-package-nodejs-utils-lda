c587bf527eff03cc7b78c077808c1815eb34ec22
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-10-10 11:57:21 -0300

    updated

---
+++ emailModule.js
@@ -0,0 +1,52 @@
+import { createTransport } from "nodemailer";
+import { fopen, fwrite } from "../modules/autoFileSysModule.js";
+const configs = fopen("config.json");
+const configMail = configs.emailSystem
+let transporter;
+
+if (createTransport({ service: configMail.service })) {
+    // Se o serviço estiver entre os suportados pelo Nodemailer, use createTransport com o serviço
+    transporter = createTransport({
+        service: configMail.service || "Gmail",
+        auth: {
+            user: process.env.email_server,
+            pass: process.env.email_password
+        }
+    });
+} else {
+    // Caso contrário, crie o transporte manualmente
+    transporter = createTransport({
+        host: configMail.host,
+        port: configMail.port,
+        secure: configMail.ssl_tls, // SSL/TLS ativado
+        auth: {
+            user: process.env.email_server,
+            pass: process.env.email_password
+        }
+    });
+}
+
+function sendMail(email, subject, text, callback) {
+    try {
+        const mailOptions = {
+            from: process.env.email_server, // Remetente
+            to: email, // Destinatário
+            subject: subject,
+            text: text,
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("SERVIDOR <sendMail>: Erro ao enviar o e-mail:", error);
+                callback(error, null);
+            } else {
+                console.log("SERVIDOR <sendMail>: E-mail enviado:", info.response);
+                callback(null, info);
+            }
+        });
+    } catch (error) {
+        console.error("SERVIDOR <sendMail>: Erro ao criar email: ", error);
+        callback(error, null);
+    }
+}
+
+export default sendMail;
---
+++ fetchModule.js
@@ -0,0 +1,127 @@
+import fetch from "node-fetch";
+
+const headersDefault = {
+  "x-forwarded-proto": "https,http,http",
+  "x-forwarded-port": "443,80,80",
+  "accept-encoding": "gzip",
+};
+
+function fetchGet(url, header, callback) {
+  const newHeaders = Object.assign(headersDefault, header);
+  const requestOptions = {
+    method: "GET",
+    headers: newHeaders,
+  };
+
+  console.log("FETCH GET:", url);
+  fetch(url, requestOptions)
+    .then((response) => {
+      console.log("Status da resposta:", response.status, response.statusText);
+      const contentType = response.headers.get("content-type");
+      console.log("Tipo de conteúdo:", contentType);
+
+      // Verifica se houve erro na resposta
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          throw new Error(`Erro na resposta do servidor: ${JSON.stringify(errorData, null, 2)}`);
+        });
+      }
+
+      // Verifica o tipo de conteúdo retornado
+      if (contentType && contentType.includes("application/json")) {
+        // Se for JSON, retorna o JSON
+        return response.json();
+      } else {
+        // Se não for JSON, retorna o conteúdo como texto
+        return response.text();
+      }
+    })
+    .then((data) => {
+      console.log("FETCH GET RECEBIDO! OK 200");
+      console.log("Dados recebidos:", data);
+      callback(null, data);
+    })
+    .catch((error) => {
+      console.error(`Erro ao fazer a requisição para ${url}: ${error}`);
+      callback(error, null);
+    });
+}
+
+function fetchPost(url, payload, header, callback) {
+  const defaultContentType = {
+    "content-type": `application/json; charset=UTF-8`,
+  };
+  var newHeaders = headersDefault;
+  newHeaders = Object.assign(headersDefault, header || defaultContentType);
+  const requestOptions = {
+    method: "POST",
+    headers: newHeaders,
+    body: payload,
+  };
+
+  if (newHeaders["content-type"] == "application/json; charset=UTF-8") {
+    console.log("Convertendo payload para JSON!");
+    requestOptions.body = JSON.stringify(payload);
+  }
+
+  console.log("FETCH POST", url);
+  fetch(url, requestOptions)
+    .then((response) => {
+      console.log("Status da resposta:", response.status, response.statusText);
+      const contentType = response.headers.get("content-type");
+      console.log("Tipo de conteúdo:", contentType);
+
+      // Verifica se houve erro na resposta
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          throw new Error(JSON.stringify(errorData, null, 2));
+        });
+      }
+
+      // // Verifica o tipo de conteúdo retornado
+      if (contentType && contentType.includes("application/json")) {
+        // Se for JSON, retorna o JSON
+        return response.json();
+      } else {
+        // Se não for JSON, retorna o conteúdo como texto
+        return response.text();
+      }
+    })
+    .then((data) => {
+      console.log("FETCH POST ENVIADO! OK 200");
+      console.log("Dados recebidos:", data);
+      callback(null, data);
+    })
+    .catch((error) => {
+      console.error(`Erro ao fazer a requisição para ${url}: ${error}`);
+      callback(error, null);
+    });
+}
+
+function discordLogs(title, mensagem) {
+  const date = new Date();
+  const ano = date.getFullYear();
+  const preSet = {
+    content: "",
+    embeds: [
+      {
+        title: `SERVIDOR/${title}`,
+        description: "SERVIDOR: " + mensagem,
+        color: parseInt("FFFFFF", 16),
+        timestamp: date, // Adiciona um timestamp atual
+        footer: {
+          text: `₢Todos os Direitos Reservados - ${ano}`,
+          icon_url: "",
+        },
+      },
+    ],
+    attachments: [],
+  };
+  fetchPost(process.env.DISCORD_LOGS_WEBHOOK_URL, preSet, null, (error, data) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+}
+
+export default { fetchGet, fetchPost, discordLogs };
