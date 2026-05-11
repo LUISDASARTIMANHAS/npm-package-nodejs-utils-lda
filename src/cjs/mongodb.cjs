d0279c3d358d39824846d54f9ab3feddbf034717
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-05-11 16:23:43 -0300

    updated

--- src/cjs/mongodb.cjs
+++ src/cjs/mongodb.cjs
@@ -1,28 +1,20 @@
 require("dotenv/config");
 const { MongoClient } = require("mongodb");
-const  discordLogs  = require("./discordUtils/discordSender.cjs");
+const { discordLogs } = require("./discordUtils/discordSender.cjs");
 
 async function mongoConnect(connectionString) {
-  let mongoClient, srv;
+  let mongoClient;
 
-  if (!connectionString) {
-    console.log("USING .ENV FILES AUTOMATICALLY!");
-    srv = process.env.MONGO_CONNECTION_STRING;
-  } else {
-    srv = connectionString;
-    console.warn(
-      "SET 'MONGO_CONNECTION_STRING' VARIABLE IN .ENV FILE FOR SECURE AND AUTOMATIC CONNECTION"
-    );
-  }
+  const AutoConnectionString = getAutoConnectionString(connectionString);
 
   try {
-    mongoClient = new MongoClient(srv);
+    mongoClient = new MongoClient(AutoConnectionString);
     discordLogs("MONGO DB CONNECTION", "Connecting to database cluster...");
     console.log("Connecting to database cluster...");
     await mongoClient.connect();
     discordLogs(
       "MONGO DB CONNECTION",
-      "Connected to MongoDB Atlas successfully!"
+      "Connected to MongoDB Atlas successfully!",
     );
     console.log("Connected to MongoDB Atlas successfully!");
 
@@ -34,22 +26,61 @@ async function mongoConnect(connectionString) {
   }
 }
 
-async function select(connection, database, table) {
+function getDatabase(client, databaseName) {
   // - Obtém o banco de dados "database" fornecido da conexão.
-  const db = connection.db(database);
+  return client.db(databaseName);
+}
+
+function getTable(database, tableName) {
   // - Obtém a coleção "collection" fornecida do banco de dados.
-  const colecao = db.collection(table);
+  return database.collection(tableName);
+}
+
+function getALLData(table) {
   // - Realiza uma consulta para encontrar todos os documentos da coleção e retorna os resultados como um array.
-  return colecao.find().toArray();
+  return table.find().toArray();
 }
 
-async function insert(connection, database, table, data) {
+function getAutoConnectionString(connectionString) {
+  let autoConnectionString = null;
+  if (!connectionString) {
+    console.log("USING .ENV FILES AUTOMATICALLY!");
+    autoConnectionString = process.env.MONGO_CONNECTION_STRING;
+  } else {
+    autoConnectionString = connectionString;
+    console.warn(
+      "SET 'MONGO_CONNECTION_STRING' VARIABLE IN .ENV FILE FOR SECURE AND AUTOMATIC CONNECTION",
+    );
+  }
+  return autoConnectionString;
+}
+
+async function select(client, databaseName, tableName) {
   // - Obtém o banco de dados "database" fornecido da conexão.
-  const db = connection.db(database);
-  // - Obtém a coleção "table" fornecido do banco de dados.
-  const colecao = db.collection(table);
-  // - Insere um novo documento (data) na coleção (table) e retorna um objeto com informações sobre a inserção.
-  return colecao.insertOne(data);
+  const db = getDatabase(client, databaseName);
+  // - Obtém a coleção "collection" fornecida do banco de dados.
+  const table = getTable(db, tableName);
+  // - Realiza uma consulta para encontrar todos os documentos da coleção e retorna os resultados como um array.
+  return getALLData(table);
 }
 
-module.exports = { mongoConnect, select, insert };
+async function selectAuto(databaseName, tableName) {
+  const connection = await mongoConnect();
+  return await select(connection, databaseName, tableName);
+}
+
+async function insert(connection, databaseName, tableName, data) {
+  // - Obtém o banco de dados "database" fornecido da conexão.
+  const db = getDatabase(client, databaseName);
+  // - Obtém a coleção "collection" fornecida do banco de dados.
+  const table = getTable(db, tableName);
+  // - Insere um novo documento (data) na coleção (table) e retorna um objeto com informações sobre a inserção.
+  return table.insertOne(data);
+}
+
+async function insertAuto(databaseName, tableName, data) {
+  const connection = await mongoConnect();
+  return await insert(connection, databaseName, tableName, data);
+}
+
+module.exports = { mongoConnect, select, selectAuto, insert, insertAuto };
--- src/esm/mongodb.mjs
+++ src/esm/mongodb.mjs
@@ -3,29 +3,17 @@ import { MongoClient } from "mongodb";
 import { discordLogs } from "./discordUtils/discordSender.mjs";
 
 export async function mongoConnect(connectionString) {
-  let mongoClient, srv;
-
-  if (!connectionString) {
-    console.log("USING .ENV FILES AUTOMATICALLY!");
-    srv = process.env.MONGO_CONNECTION_STRING;
-  } else {
-    srv = connectionString;
-    console.warn(
-      "SET 'MONGO_CONNECTION_STRING' VARIABLE IN .ENV FILE FOR SECURE AND AUTOMATIC CONNECTION"
-    );
-  }
+  let mongoClient;
+  const AutoConnectionString = getAutoConnectionString(connectionString);
 
   try {
-    mongoClient = new MongoClient(srv);
-    discordLogs(
-      "MONGO DB CONNECTION",
-      "Connecting to database cluster..."
-    );
+    mongoClient = new MongoClient(AutoConnectionString);
+    discordLogs("MONGO DB CONNECTION", "Connecting to database cluster...");
     console.log("Connecting to database cluster...");
     await mongoClient.connect();
     discordLogs(
       "MONGO DB CONNECTION",
-      "Connected to MongoDB Atlas successfully!"
+      "Connected to MongoDB Atlas successfully!",
     );
     console.log("Connected to MongoDB Atlas successfully!");
 
@@ -37,21 +25,59 @@ export async function mongoConnect(connectionString) {
   }
 }
 
-
-export async function select(connection, database, table) {
+function getDatabase(client, databaseName) {
   // - Obtém o banco de dados "database" fornecido da conexão.
-  const db = connection.db(database);
-  // - Obtém a coleção "collection" fornecida do banco de dados.
-  const colecao = db.collection(table);
-  // - Realiza uma consulta para encontrar todos os documentos da coleção e retorna os resultados como um array.
-  return colecao.find().toArray();
+  return client.db(databaseName);
 }
 
-export async function insert(connection, database, table, data) {
+function getTable(database, tableName) {
+  // - Obtém a coleção "collection" fornecida do banco de dados.
+  return database.collection(tableName);
+}
+
+function getALLData(table) {
+  // - Realiza uma consulta para encontrar todos os documentos da coleção e retorna os resultados como um array.
+  return table.find().toArray();
+}
+
+function getAutoConnectionString(connectionString) {
+  let autoConnectionString = null;
+  if (!connectionString) {
+    console.log("USING .ENV FILES AUTOMATICALLY!");
+    autoConnectionString = process.env.MONGO_CONNECTION_STRING;
+  } else {
+    autoConnectionString = connectionString;
+    console.warn(
+      "SET 'MONGO_CONNECTION_STRING' VARIABLE IN .ENV FILE FOR SECURE AND AUTOMATIC CONNECTION",
+    );
+  }
+  return autoConnectionString;
+}
+
+export async function select(connection, databaseName, tableName) {
   // - Obtém o banco de dados "database" fornecido da conexão.
-  const db = connection.db(database);
-  // - Obtém a coleção "table" fornecido do banco de dados.
-  const colecao = db.collection(table);
+  const db = getDatabase(client, databaseName);
+  // - Obtém a coleção "collection" fornecida do banco de dados.
+  const table = getTable(db, tableName);
+  // - Realiza uma consulta para encontrar todos os documentos da coleção e retorna os resultados como um array.
+  return table.find().toArray();
+}
+
+export async function selectAuto(databaseName, tableName) {
+  const connection = await mongoConnect();
+  return await select(connection, databaseName, tableName);
+}
+
+export async function insert(connection, databaseName, tableName, data) {
+  // - Obtém o banco de dados "database" fornecido da conexão.
+  const db = getDatabase(client, databaseName);
+  // - Obtém a coleção "collection" fornecida do banco de dados.
+  const table = getTable(db, tableName);
   // - Insere um novo documento (data) na coleção (table) e retorna um objeto com informações sobre a inserção.
-  return colecao.insertOne(data);
+  return table.insertOne(data);
+}
+
+export async function insertAuto(databaseName, tableName, data) {
+  const connection = await mongoConnect();
+  return await insert(connection, databaseName, tableName, data);
 }
