ddb97b01940efb419fb8e75075b2822d0b268be2
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-09-19 11:29:05 -0300

    updated tokens

--- src/scripts/updateToken.cmd
+++ src/scripts/updateToken.cmd
@@ -1,3 +1,3 @@
 @echo off
 echo //registry.npmjs.org/:_authToken=%npm_token%
-npm config set //registry.npmjs.org/:_authToken=%npm_token%
+npm config delete //registry.npmjs.org/:_authToken && npm config set //registry.npmjs.org/:_authToken=%npm_token% && npm whoami
